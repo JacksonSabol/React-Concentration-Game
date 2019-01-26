fda858c5038152c9b16b2ce92b7dd58b68080f83
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2019-01-25 20:51:51 -0800

    add React component card images to assets; change file paths to match in cards JSON; update styling of header, footer, wrapper, cards, and body to match 12 tile set up of concentration game and be mobile responsive - footer CSS needs edit to be mobile responsive

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./Footer.css";
+import "./style.css";
 
 const Footer = () => (
     <footer className="footer">
-        <div className="text-center">
-            Build with React.js to help visually demonstrate the mental process of keeping track of React components.
+        <div className="bottom">
+            Built with React.js to help visually demonstrate the mental process of keeping track of React components.
         </div>
     </footer>
 )
