249fcce1899c26ac5abb380fa79618b10df9029e
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2019-01-26 18:55:04 -0800

    Updates

---
+++ precache-manifest.a8ebcf3355972020be4c111e10eef924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa065a4fa2b6fefe7baaa9188970eb61",
+    "url": "/React-Concentration-Game/static/media/InstructotronBackground.fa065a4f.jpg"
+  },
+  {
+    "revision": "eb4a42e5cb3bd3d51d40",
+    "url": "/React-Concentration-Game/static/js/runtime~main.eb4a42e5.js"
+  },
+  {
+    "revision": "0cd2d9e5b5acb09f34e4",
+    "url": "/React-Concentration-Game/static/js/main.0cd2d9e5.chunk.js"
+  },
+  {
+    "revision": "7644a9751e730a17c6a5",
+    "url": "/React-Concentration-Game/static/js/1.7644a975.chunk.js"
+  },
+  {
+    "revision": "0cd2d9e5b5acb09f34e4",
+    "url": "/React-Concentration-Game/static/css/main.7811f3fb.chunk.css"
+  },
+  {
+    "revision": "c33ee709e4adb256eb6340fbb189cdf4",
+    "url": "/React-Concentration-Game/index.html"
+  }
+];
